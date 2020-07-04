746d08144d1cf69ecc8b686af20a78e8504acd2c
MinexAutomation <minex.automation@gmail.com>
2020-07-04 00:35:54 -0700

    Added R5T.D0026.Default library. Added OSPlatformSwitch service implementation class type.

---
+++ source/R5T.D0026.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.D0024;
+
+using R5T.Dacia;
+
+using IParameterizedOSPlatformSwitch = R5T.D0025.IOSPlatformSwitch;
+
+
+namespace R5T.D0026.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="OSPlatformSwitch"/> implementation of <see cref="IOSPlatformSwitch"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddOSPlatformSwitch(this IServiceCollection services,
+            IServiceAction<IOSPlatformProvider> oSPlatformProviderAction,
+            IServiceAction<IParameterizedOSPlatformSwitch> parameterizedOSPlatformSwitchAction)
+        {
+            services
+                .AddSingleton<IOSPlatformSwitch, OSPlatformSwitch>()
+                .Run(oSPlatformProviderAction)
+                .Run(parameterizedOSPlatformSwitchAction)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="OSPlatformSwitch"/> implementation of <see cref="IOSPlatformSwitch"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IOSPlatformSwitch> AddOSPlatformSwitchAction(this IServiceCollection services,
+            IServiceAction<IOSPlatformProvider> oSPlatformProviderAction,
+            IServiceAction<IParameterizedOSPlatformSwitch> parameterizedOSPlatformSwitchAction)
+        {
+            var serviceAction = ServiceAction.New<IOSPlatformSwitch>(() => services.AddOSPlatformSwitch(
+                oSPlatformProviderAction,
+                parameterizedOSPlatformSwitchAction));
+
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.D0026.Default/Code/Services/Implementations/OSPlatformSwitch.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.D0024;
+
+using IParameterizedOSPlatformSwitch = R5T.D0025.IOSPlatformSwitch;
+
+
+namespace R5T.D0026.Default
+{
+    public class OSPlatformSwitch : IOSPlatformSwitch
+    {
+        private IOSPlatformProvider OSPlatformProvider { get; }
+        private IParameterizedOSPlatformSwitch ParameterizedOSPlatformSwitch { get; }
+
+
+        public OSPlatformSwitch(
+            IOSPlatformProvider oSPlatformProvider,
+            IParameterizedOSPlatformSwitch parameterizedOSPlatformSwitch)
+        {
+            this.OSPlatformProvider = oSPlatformProvider;
+            this.ParameterizedOSPlatformSwitch = parameterizedOSPlatformSwitch;
+        }
+
+        public void Switch(Action windowsAction, Action osxAction, Action linuxAction)
+        {
+            var oSPlatform = this.OSPlatformProvider.GetOSPlatform();
+
+            this.ParameterizedOSPlatformSwitch.Switch(oSPlatform, windowsAction, osxAction, linuxAction);
+        }
+
+        public Task SwitchAsync(Func<Task> windowsActionAsync, Func<Task> osxActionAsync, Func<Task> linuxActionAsync)
+        {
+            var oSPlatform = this.OSPlatformProvider.GetOSPlatform();
+
+            var task = this.ParameterizedOSPlatformSwitch.SwitchAsync(oSPlatform, windowsActionAsync, osxActionAsync, linuxActionAsync);
+            return task;
+        }
+    }
+}
